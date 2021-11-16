754c56739c2317d9fb916c860d87594582029fe5
barotiv <88926689+barotiv@users.noreply.github.com>
2021-11-16 21:45:22 +0100

    Fulfilled

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,7 @@
+const setCookie = (string) => {
+    const dateExpires = new Date();
+    dateExpires.setTime(dateExpires.getTime + (1000 * 60 * 15));
+    document.cookie = `token=${string}; expires=${dateExpires}`;
+}
 
+export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,24 @@
+const cookieHandler = {
+    getAll() {
+        const cookies = document.cookie.split("; ").map((item) => item.split("="));
+        return Object.fromEntries(cookies);
+    },
 
+    toSessionStorage() {
+        const cookies = this.getAll();
+        for (let key in cookies) {
+            sessionStorage.setItem(key, cookies[key]);
+        }
+    },
+
+    flush() {
+        const cookies = document.cookie.split("; ").map((item) => item.split("="));
+        cookies.forEach(
+            (item) => (document.cookie = `${item[0]}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`)
+        );
+    },
+};
+
+export {
+    cookieHandler
+};
